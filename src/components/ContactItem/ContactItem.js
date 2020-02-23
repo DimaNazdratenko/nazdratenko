425f029fb986fb2be0d8a8c0f6f1c6aff88e0f76
Dima Nazdratenko <dima.nazdratenko@gmail.com>
2020-02-23 22:07:18 +0200

    refactor - add 'ContactItem' component and refactor 'Contacts' component

---
+++ src/components/ContactItem/ContactItem.js
@@ -0,0 +1,53 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import styled, { keyframes } from 'styled-components';
+import {
+  rollIn,
+  fadeInRight,
+  fadeInDown
+} from 'react-animations';
+import './ContactItem.scss';
+
+const RollInAnimation = styled.div`animation: 1s ${keyframes`${rollIn}`}`;
+const FadeInRightAnimation = styled.div`animation: 1s ${keyframes`${fadeInRight}`}`;
+const FadeInDownAnimation = styled.div`animation: 1s ${keyframes`${fadeInDown}`}`;
+
+const ContactItem = (props) => {
+  const {
+    name,
+    href,
+    newTab,
+    text1,
+    text2
+  } = props;
+
+  const srcToImg = `assets/site/social_icons/${name}.svg`;
+
+  return (
+    <div className="contacts_item">
+      <RollInAnimation>
+        <a className="contacts_item_link" href={href} target={newTab ? '_blank' : undefined} rel="noopener noreferrer">
+          <img src={srcToImg} alt={name} />
+        </a>
+      </RollInAnimation>
+
+      <FadeInRightAnimation>
+        <div className="contacts_item_text1">{text1}</div>
+      </FadeInRightAnimation>
+
+      <FadeInDownAnimation>
+        <span className="contacts_item_text2">{text2}</span>
+      </FadeInDownAnimation>
+    </div>
+  );
+};
+
+ContactItem.propTypes = {
+  name: PropTypes.string.isRequired,
+  href: PropTypes.string.isRequired,
+  newTab: PropTypes.bool.isRequired,
+  text1: PropTypes.string.isRequired,
+  text2: PropTypes.string.isRequired
+};
+
+export default ContactItem;
--- src/components/Contacts/Contacts.js
+++ src/components/Contacts/Contacts.js
@@ -2,119 +2,78 @@ import React from 'react';
 import styled, { keyframes } from 'styled-components';
 import {
   tada,
-  rollIn,
-  fadeInRight,
-  fadeInDown
 } from 'react-animations';
+import ContactItem from '../ContactItem/ContactItem';
 import './Contacts.scss';
 
 const TadaAnimation = styled.div`animation: 1s ${keyframes`${tada}`}`;
-const RollInAnimation = styled.div`animation: 1s ${keyframes`${rollIn}`}`;
-const FadeInRightAnimation = styled.div`animation: 1s ${keyframes`${fadeInRight}`}`;
-const FadeInDownAnimation = styled.div`animation: 1s ${keyframes`${fadeInDown}`}`;
 
 const Contacts = () => {
+  const itemsInfo = [
+    {
+      name: 'location',
+      href: 'https://www.google.com.ua/maps/place/Kiev',
+      newTab: true,
+      text1: 'Location:',
+      text2: 'Kiev'
+    },
+    {
+      name: 'phone',
+      href: 'tel:+380638419651',
+      newTab: false,
+      text1: 'Phone:',
+      text2: '+380 (63) 841 96 51'
+    },
+    {
+      name: 'mail',
+      href: 'mailto: dima.nazdratenko@gmail.com',
+      newTab: false,
+      text1: 'Mail:',
+      text2: 'dima.nazdratenko@gmail.com'
+    },
+    {
+      name: 'skype',
+      href: 'skype:hitman3322?chat',
+      newTab: false,
+      text1: 'Skype:',
+      text2: 'hitman3322'
+    },
+    {
+      name: 'telegram',
+      href: 'https://telegram.me/DimaNazdratenko',
+      newTab: true,
+      text1: 'Telegram:',
+      text2: '@DimaNazdratenko'
+    },
+    {
+      name: 'messenger',
+      href: 'https://www.messenger.com/t/DimaNazdratenko',
+      newTab: true,
+      text1: 'Messenger:',
+      text2: 'DimaNazdratenko'
+    }
+  ];
+
+  const listItems = itemsInfo.map((item) => {
+    return (
+      <ContactItem
+        key={item.name}
+        name={item.name}
+        href={item.href}
+        newTab={item.newTab}
+        text1={item.text1}
+        text2={item.text2}
+      />
+    );
+  });
+
   return (
     <div className="contacts">
       <TadaAnimation>
         <h1 className="contacts_title">Contact me</h1>
       </TadaAnimation>
 
-      <div className="contacts_item">
-        <RollInAnimation>
-          <a className="contacts_item_link" href="https://www.google.com.ua/maps/place/Kiev" target="_blank" rel="noopener noreferrer">
-            <img src="assets/site/social_icons/location.svg" alt="location" />
-          </a>
-        </RollInAnimation>
-
-        <FadeInRightAnimation>
-          <div className="contacts_item_text1">Location:</div>
-        </FadeInRightAnimation>
-
-        <FadeInDownAnimation>
-          <span className="contacts_item_text2">Kiev</span>
-        </FadeInDownAnimation>
-      </div>
-
-      <div className="contacts_item">
-        <RollInAnimation>
-          <a className="contacts_item_link" href="tel:+380638419651">
-            <img src="assets/site/social_icons/phone.svg" alt="phone" />
-          </a>
-        </RollInAnimation>
-
-        <FadeInRightAnimation>
-          <div className="contacts_item_text1">Phone:</div>
-        </FadeInRightAnimation>
-
-        <FadeInDownAnimation>
-          <span className="contacts_item_text2">+380 (63) 841 96 51</span>
-        </FadeInDownAnimation>
-      </div>
-
-      <div className="contacts_item">
-        <RollInAnimation>
-          <a className="contacts_item_link" href="mailto: dima.nazdratenko@gmail.com">
-            <img src="assets/site/social_icons/mail.svg" alt="mail" />
-          </a>
-        </RollInAnimation>
-
-        <FadeInRightAnimation>
-          <div className="contacts_item_text1">Mail:</div>
-        </FadeInRightAnimation>
-
-        <FadeInDownAnimation>
-          <span className="contacts_item_text2">dima.nazdratenko@gmail.com</span>
-        </FadeInDownAnimation>
-      </div>
-
-      <div className="contacts_item">
-        <RollInAnimation>
-          <a className="contacts_item_link" href="skype:hitman3322?chat">
-            <img src="assets/site/social_icons/skype.svg" alt="skype" />
-          </a>
-        </RollInAnimation>
-
-        <FadeInRightAnimation>
-          <div className="contacts_item_text1">Skype:</div>
-        </FadeInRightAnimation>
-
-        <FadeInDownAnimation>
-          <span className="contacts_item_text2">hitman3322</span>
-        </FadeInDownAnimation>
-      </div>
-
-      <div className="contacts_item">
-        <RollInAnimation>
-          <a className="contacts_item_link" href="https://telegram.me/DimaNazdratenko" target="_blank" rel="noopener noreferrer">
-            <img src="assets/site/social_icons/telegram.svg" alt="telegram" />
-          </a>
-        </RollInAnimation>
-
-        <FadeInRightAnimation>
-          <div className="contacts_item_text1">Telegram:</div>
-        </FadeInRightAnimation>
-
-        <FadeInDownAnimation>
-          <span className="contacts_item_text2">@DimaNazdratenko</span>
-        </FadeInDownAnimation>
-      </div>
-
-      <div className="contacts_item">
-        <RollInAnimation>
-          <a className="contacts_item_link" href="https://www.messenger.com/t/DimaNazdratenko" target="_blank" rel="noopener noreferrer">
-            <img src="assets/site/social_icons/messenger.svg" alt="messenger" />
-          </a>
-        </RollInAnimation>
-
-        <FadeInRightAnimation>
-          <div className="contacts_item_text1">Messenger:</div>
-        </FadeInRightAnimation>
-
-        <FadeInDownAnimation>
-          <span className="contacts_item_text2">DimaNazdratenko</span>
-        </FadeInDownAnimation>
-      </div>
+      {listItems}
     </div>
   );
 };
